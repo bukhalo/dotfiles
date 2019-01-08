ee3bd8de2f00132dcdbffe38b091a7c28683248d
Aleksandr Bukhalo <a@bukhalo.com>
2019-01-08 21:39:23 +0300

    Added bash script for make symlinks into home folder

---
+++ make-symlinks.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# define variables
+HOME_DIR=$HOME
+CURRENT_DIR=$PWD
+
+# make symbolic link to home directory
+make_symlink () {
+  ln -s "$CURRENT_DIR/$1" "$HOME_DIR/$1"
+}
+
+make_symlink .gitconfig
